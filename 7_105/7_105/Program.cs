b17ef31989bfd86c8385a3c797ca0bc208b2775f
Syar3005 <113889600+Syar3005@users.noreply.github.com>
2023-02-12 22:09:01 +0300

    Add files via upload

---
+++ 7_105/7_105/Program.cs
@@ -0,0 +1,26 @@
+﻿Random rnd = new Random();
+
+Console.WriteLine("Введите количество элементов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+int[] c = new int[n];
+
+for (int i = 0; i < n; i++)
+{
+    c[i] = rnd.Next(50);
+
+	if (c[i] < 20)
+	{
+		count++;
+	}
+}
+Console.WriteLine($"{count} элементов массива, меньших 20");
+if(count == 5)
+{
+	Console.WriteLine(true);
+}
+else
+{
+    Console.WriteLine(false);
+}
